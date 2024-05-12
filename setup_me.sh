b7ebcef9281cab0a0a541fc1254813bd47e002f8
sudohdl <111178236+sudohdl@users.noreply.github.com>
2024-05-12 12:07:38 +0300

    Create setup_me.sh

---
+++ setup_me.sh
@@ -0,0 +1,11 @@
+#MGLS 
+export MGLS_HOME=/home/debian/microchip/ModelSimPro/modeltech/linuxacoem/mgls
+export MGLS_LICENCE_FILE=1702@debian-MacBookAir
+
+export ME_HOME=/home/debian/microchip/ModelSimPro/modeltech/linuxacoem/
+export PATH=$ME_HOME:$PATH
+
+echo "******************************************************************"
+echo "XXXXXXXXXXXXXXXXXOOOOOOOOOOOOOOOOOOOOOOOOOOOOXXXXXXXXXXXXXXXXXXXXX"
+echo "******************************************************************"
+
